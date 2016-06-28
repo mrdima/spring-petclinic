20301da114e39a81522bae6ba4c1880d4476d7e8
Mirnes Omerkic <mirnes.omerkic@atlantbh.com>
2016-06-28 12:57:51 +0200

    Add create/push app image

--- build.sh
+++ build.sh
@@ -7,7 +7,11 @@ BRANCH="mysql"
 
 BUILD_IMAGE="momerkic/app-build:v2.2"
 BUILD_CONTAINER="app-build"
-DOCKERFILE="./Dockerfile.build"
+BUILD_DOCKERFILE="./Dockerfile.build"
+
+APP_IMAGE="momerkic/app-web:v2.2"
+APP_CONTAINER="app-web"
+#APP_DOCKERFILE="./Dockerfile"
 
 # Clone repo
 salt $DOCKER_HOST cmd.run "/bin/rm -rf $BUILD_PATH; \
@@ -17,4 +21,14 @@ salt $DOCKER_HOST cmd.run "/bin/rm -rf $BUILD_PATH; \
 # Create build container
 salt $DOCKER_HOST cmd.run "docker rmi -f $BUILD_IMAGE; \
   docker rm -f $BUILD_CONTAINER; \
-  cd $BUILD_PATH && docker build -t $BUILD_CONTAINER -f $DOCKERFILE . "
+  cd $BUILD_PATH && docker build -t $BUILD_IMAGE -f $BUILD_DOCKERFILE . "
+
+# Copy war
+salt $DOCKER_HOST cmd.run "docker create --name $BUILD_CONTAINER $BUILD_IMAGE; \
+  cd $BUILD_PATH && docker cp $BUILD_CONTAINER:/tmp/app-build/target/petclinic.war . ; \
+  docker rm -f $BUILD_CONTAINER "
+
+# Create/push app image
+salt $DOCKER_HOST cmd.run "docker rmi -f $APP_IMAGE; \
+  docker rm -f $APP_CONTAINER; \
+  cd $BUILD_PATH && docker build -t $APP_IMAGE . && docker push $APP_IMAGE "
